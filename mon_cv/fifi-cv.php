2e39b8f6475ca58325d1afb9142980c4ce78b4d7
fifi634 <cohen.philippe@lilo.org>
2022-02-09 19:53:18 +0100

    feat(mon cv) : create CV in html and css

---
+++ mon_cv/fifi-cv.php
@@ -0,0 +1,157 @@
+<?php
+    $title = "CV de Philippe Cohen";
+    require '../elements/header.php';
+?>
+    <link rel="stylesheet" type="text/css" href="style-cv.css" />
+    
+    
+        
+    <!-- Identitée en En-Tête-->
+        
+        <header class="header">
+            <h1>Philippe Cohen</h1><br />
+        
+            <a class="a_cv" href="images/photo_cv.jpg" title="cliquer pour agrandir"><img src="images/photo_cv_mini.jpg" alt="Photo portrait de Philippe Cohen" class="identity"></a>
+    
+            <ul class="identity ul-cv">
+                <li>34 ans</li>
+                <li>Nationalité française</li>
+                <li>06 98 19 09 76 / cohen.philippe@lilo.org</li>
+            </ul><br />
+        </header>    
+
+    <!-- Menu -->
+        <nav>
+        </nav>    
+
+        
+    <!-- Compétences -->
+    
+        <section class=compétence>
+            <h2 class="corp">Compétences</h2>
+    
+            <ul class="ul-cv">
+                <li><strong class="strong_cv">Informatique :</strong> support IT; montage audio ; adiministration réseau ; Windows ; Ubuntu ; Debian ; Nextcloud ; Raspberry ; suites bureautique </li><br />
+            
+                <li><strong class="strong_cv">Logiciels maîtrisés :</strong> WinMedia, Netia Radio Assist, Selector, Samplitude, Pro Tools, Sound Forge, Audition, Audacity, Première, Photoshop, Gimp, Libre Office, Microsoft Office, WordPress, Web Acapella</li><br />
+            
+                <li><strong class="strong_cv">Scène :</strong> régie son, vidéo, lumiére, poursuite ; montage ; choix du matériel ; plan feu ; fiche technique ; balances</li><br />
+            
+                <li><strong class="strong_cv">Animation radio :</strong> animation de flux ; voice track ; voix antenne</li><br />
+            
+                <li><strong class="strong_cv">Montage :</strong> audio, vidéo, photo (notion), mastering audio ; prise de son</li><br />
+            
+                <li><strong class="strong_cv">Technique radio :</strong> Réalisation tous type d'émissions ; conception et maintenance de studio ; duplexe via 4g ; liaisons par IP ; émetteur fm (notion)</li><br />
+            
+                <li><strong class="strong_cv">Interpersonnelles :</strong> adaptabilité ; gestion du temps ; organisation ; esprit d'équipe</li>
+            </ul><br />
+        </section>
+        
+        
+    <!-- Expériences -->
+        
+        <section>
+            <h2>Expériences</h2>
+    
+            <ul class="ul-cv">
+                <li>
+                    <div class="date">2018 à 2021 :</div>
+                    <strong class="strong_cv">Initiative</strong> associtation d'éducation populaire (63)<br />
+                    co-fondateur, gestion administrative, webmaster, support IT<br />
+                    <a class="a_cv"  href="https://initiative.ovh" target=_blank>inititiative.ovh</a>
+                </li><br />
+        
+                <li>
+                    <div class="date">2017 à ce jour :</div>
+                    <strong class="strong_cv">la Radio de Fifi</strong> webradio muscale<br />Fondateur, Programmateur, Monteur, Voix antenne, Webmaster, Media Planner<br />
+                    <a class="a_cv"  href="https://laradiodefifi.fr" target=_blank>laradiodefifi.fr</a>"
+                </li><br />
+        
+                <li>
+                    <div class="date">2012 à ce jour :</div>
+                    <strong class="strong_cv">Radio Arverne</strong> radio fm (63)<br /> 
+                    Technicien, Réalisateur, Webmaster, Support IT, Monteur, Voix antenne <br />
+                    <a class="a_cv"  href="https://www.radioarverne.com" target=_blank>radioarverne.com</a>
+                </li><br />
+    
+                <li><div class="date">2011 à 2012</div>
+                    <strong class="strong_cv">Le Calypso</strong> camping-club (66)<br />
+                    Régisseur son et lumière
+                </li><br />
+        
+                <li>
+                    <div class="date">2008 à 2010</div>
+                    <strong class="strong_cv">Look Voyage</strong> club Lookéa à Corfu (Grèce), La Somone (Sénégal), Marrakech (Maroc). <br />
+                    Technicien son, lumière et vidéo ; Disc-Jockey
+                </li><br />
+        
+                <li> 
+                    <div class="date">2006 à 2007</div> <strong class="strong_cv">Littoral Fm</strong> radio fm (66)<br />
+                    Technicien, Réalisateur, Media Planner
+                </li><br />
+        
+                <li>
+                    <div class="date">2004 à 2009</div>
+                    <strong class="strong_cv">Radio Zygomar</strong> radio fm (66)<br />
+                    Technicien, Réalisateur, Programmateur, Animateur (matinal, mix, talk show)
+                </li><br />
+        
+                <li><div class="date">2004 à 2006</div>
+                    <strong class="strong_cv">Disc-Jockey</strong> dans différents bars et boîte de nuit (66)
+                </li>
+            </ul><br />
+        </section>
+        
+    <!-- Formation -->
+            
+        <section>        
+            <h2>Formations</h2>
+    
+            <ul class="ul-cv">
+                <li> 
+                    <div class="date">Depuis 2021</div>
+                    <strong class="strong_cv">Développeur Web</strong>OpenClassRooms (en ligne)
+                </li><br />
+        
+                <li>
+                    <div class="date">2016</div>
+                    <strong class="strong_cv">BTS Audiovisuel</strong> GRETA du Velay (43)
+                </li><br />
+        
+                <li>
+                    <div class="date">2008</div>
+                    <strong class="strong_cv">Technicien du spectacle</strong> The Village (12)
+                </li><br />
+        
+                <li><div class="date">2007</div>
+                    <strong class="strong_cv">Formation supérieur aux métiers de la radio</strong> One School (11)
+                </li><br />
+        
+                <li>
+                    <div class="date">2005</div>
+                    <strong class="strong_cv">BAC STI Génie Electrotechnique</strong>Lycée Clos Banet (66)
+                </li>
+            </ul><br />
+        </section>
+        
+    <!-- Divers -->
+        <section>
+            <h2>Divers</h2>
+        
+            <ul class="ul-cv">
+                <li><strong class="strong_cv">Langue : </strong>anglais (lu, écrit, parlé) ; espagnol (castillant, notion)</li><br />
+                <li><strong class="strong_cv">Centre d'intérêt : </strong>voyages, musique, informatique et nouvelles technologies</li><br />
+                <li><strong class="strong_cv">Permi B</strong></li>
+            </ul>
+        </section>
+
+    
+    <!-- Pied de page -->
+        <footer>
+        <?php require '../elements/footer.php' ?>
+        </footer>
+        
+        
+    
+        
+    
